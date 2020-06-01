70b27dcec22c0266242d6140287dc6e71fd7b843
Reut Raman <soolamis@vivaldi.net>
2020-06-01 16:56:37 +0300

    fix

---
+++ build/precache-manifest.51002f3f3a664121c66672c772f6d061.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db975a1f4da9a9e2d73d574e37cb8a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b75c8db1f3cef1321e8",
+    "url": "/static/css/13.0fdbf87c.chunk.css"
+  },
+  {
+    "revision": "3e4bb57d3e24fea4f996",
+    "url": "/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "2b75c8db1f3cef1321e8",
+    "url": "/static/js/13.dbcd5ac3.chunk.js"
+  },
+  {
+    "revision": "e76a22a39c917efe59883bb61ee192f5",
+    "url": "/static/js/13.dbcd5ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4b0587361239e940c4",
+    "url": "/static/js/14.8bab6431.chunk.js"
+  },
+  {
+    "revision": "3e4bb57d3e24fea4f996",
+    "url": "/static/js/main.0f46719c.chunk.js"
+  },
+  {
+    "revision": "4c7972e93bbfbd1af06c",
+    "url": "/static/js/reactPlayerDailyMotion.0659a21d.chunk.js"
+  },
+  {
+    "revision": "bdffdad1e068f0e7e155",
+    "url": "/static/js/reactPlayerFacebook.e6bfef56.chunk.js"
+  },
+  {
+    "revision": "1791c5bf2dff81dd4f96",
+    "url": "/static/js/reactPlayerFilePlayer.dd002796.chunk.js"
+  },
+  {
+    "revision": "64b6dfc562d94d84c211",
+    "url": "/static/js/reactPlayerMixcloud.de170db6.chunk.js"
+  },
+  {
+    "revision": "44032e8692443f13ceb4",
+    "url": "/static/js/reactPlayerSoundCloud.f44c648f.chunk.js"
+  },
+  {
+    "revision": "a1ab0fcc2e863e4d1c83",
+    "url": "/static/js/reactPlayerStreamable.d45e8356.chunk.js"
+  },
+  {
+    "revision": "118dd2b1fe8c1d032039",
+    "url": "/static/js/reactPlayerTwitch.d1487bef.chunk.js"
+  },
+  {
+    "revision": "77d967664590e8f8ff53",
+    "url": "/static/js/reactPlayerVidyard.3d96b42c.chunk.js"
+  },
+  {
+    "revision": "d14cc3bc32a9626746da",
+    "url": "/static/js/reactPlayerVimeo.075ded62.chunk.js"
+  },
+  {
+    "revision": "63068837b33168f437bd",
+    "url": "/static/js/reactPlayerWistia.c8177bfe.chunk.js"
+  },
+  {
+    "revision": "e33429b74fe27b4aecb0",
+    "url": "/static/js/reactPlayerYouTube.06e9a57b.chunk.js"
+  },
+  {
+    "revision": "197acf65f9baad172964",
+    "url": "/static/js/runtime-main.3d943919.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07c108214bfd61363361c9e07b0a986f.js"
+  "/precache-manifest.51002f3f3a664121c66672c772f6d061.js"
 );
 
 self.addEventListener('message', (event) => {
